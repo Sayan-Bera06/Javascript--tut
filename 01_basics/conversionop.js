7f655333254feffe4eee9ed4dec0a402e66eb236
Sayan Bera <132321762+Sayan-Bera06@users.noreply.github.com>
2024-08-22 12:43:46 +0000

    added

--- 01_basics/conversionop.js
+++ 01_basics/conversionop.js
@@ -2,3 +2,8 @@ let some=99;
 let stringnum=String(some);
 console.log(stringnum);
 console.log(typeof stringnum);
+
+
+// ***********************operations*************************
+
+
---
+++ 01_basics/string.js
@@ -0,0 +1,7 @@
+let name="sayan"
+
+let roll=8388
+
+console.log(`hello my name is ${name} ,my roll is ${roll}`)
+
+console.log(name[0])
