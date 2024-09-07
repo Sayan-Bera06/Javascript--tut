249e3344354cbebc7cd4f4e2f0ba497c285245f9
Sayan Bera <132321762+Sayan-Bera06@users.noreply.github.com>
2024-09-07 14:34:22 +0000

    add

---
+++ 02_basics/map_array.js
@@ -0,0 +1,19 @@
+const programming=["sj","uj","sayan","is "]
+
+//syntax of for in loop
+
+// for(const key in programming)
+// {
+//     console.log(key);
+// }
+
+//syntax of foreach loop
+
+const values=programming.forEach((item)=>{
+
+    console.log(item)
+
+    return item;
+})
+
+console.log(values)
